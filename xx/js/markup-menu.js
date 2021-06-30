8241c5b0f798595d993e910f71740e3817919f83
VictoriaPlis <victoriyanew@gmail.com>
2021-06-30 22:52:00 +0300

    hw-10-3

--- xx/js/markup-menu.js
+++
@@ -1,8 +0,0 @@
-import menuCardsTempl from '../templates/menu-items';
-import menuItemsData from '../menu.json';
-
-const menuRef = document.querySelector('.js-menu');
-
-const menuItemsEls = menuCardsTempl(menuItemsData);
-
-menuRef.insertAdjacentHTML('beforeend', MenuItemsEls);
